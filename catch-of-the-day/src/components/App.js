b63e0c705eaa8c883bc26ced6520f0bb67bc7363
ThauNguyen <hoangthau627@gmail.com>
2018-06-19 07:51:04 +0700

    Add new files

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+
+class App extends React.Component {
+
+  addFish = (event) => {
+
+  }
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
+    );
+  }
+}
+
+export default App;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+import './css/style.css';
+import App from './components/App';
+import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker';
 
-render(<StorePicker />, document.querySelector('#main'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={StorePicker} />
+        <Route path="/store/:storeId" component={App} />
+        <Route component={NotFound} />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+render(<Root />, document.querySelector('#main'));
